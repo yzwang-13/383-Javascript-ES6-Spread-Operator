c3804f99c24b956a06d69617091c78abe79e9f36
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-14 16:29:11 +1100

    final

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// spread operators
+const citrus = ["Lime", "Lemon", "orange"];
+const fruits = ["apple", ...citrus, "banana", "coconut"];
+console.log(fruits);
+
+const fullName = {
+  fName: "James",
+  lName: "Bond"
+};
+
+const user = {
+  ...fullName,
+  id: 1,
+  username: "jamesbond007"
+};
+
+console.log(user);
